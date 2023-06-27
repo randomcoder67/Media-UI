71b0dfa3fbac86a7922a877c1bde3732f573626f
randomcoder67 <minecraft1111311@gmail.com>
2023-06-27 22:15:28 +0100

    moved magic numbers into seperate file

---
+++ pathsAndStuff.h
@@ -0,0 +1,7 @@
+#define CURRENT_SAVE_PATH "/Programs/output/updated/current.json"
+#define ALL_GAMES_JSON_PATH "/Programs/output/updated/games.json"
+#define COVER_ART_PATH "/Programs/output/.covers/bmpResized/"
+#define COVER_ART_EXTENSION ".bmp"
+#define NUM_GENRES 14
+#define MAX_CURRENT_GAMES 50
+#define MAX_GAMES_PER_GENRE 50
